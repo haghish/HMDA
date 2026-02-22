0d2144a5fa367602b0d11ff224119150da712728
E. F. Haghish <haghish.e.f@gmail.com>
2026-02-22 15:10:43 +0100

    0.2.4.1

--- R/hmda.row.plot.R
+++ R/hmda.row.plot.R
@@ -9,8 +9,15 @@
 #'               or hmda.wmshap function
 #' @param row_index Integer (length 1). The row/subject identifier to visualize. This is
 #'                  matched against the \code{index} column in \code{shapley$results}.
+#' @param top_n_features Integer. If specified, the top n features with the
+#'                       highest weighted SHAP values will be selected. This
+#'                       will be overrulled by the 'features' argument.
 #' @param features Optional character vector of feature names to plot. If \code{NULL},
 #'                 all available features in \code{shapley$results} are used.
+#'                 Specifying the \code{features} argument will override the
+#'                 \code{top_n_features} argument.
+#' @param nonzeroCI Logical. If \code{TRUE}, it avoids ploting features that have
+#'                  a confidence interval crossing zero.
 #' @param plot Logical. If \code{TRUE}, prints the plot.
 #' @param print Logical. If \code{TRUE}, prints the computed summary table for the row.
 #' @return a list including the GGPLOT2 object and the data frame of WMSHAP summary values.
@@ -72,13 +79,17 @@
 
 hmda.row.plot <- function(wmshap,
                           row_index,
+                          top_n_features = NULL,
                           features = NULL,
+                          nonzeroCI = FALSE,
                           plot = TRUE,
                           print = FALSE) {
 
   shapley.row.plot(wmshap,
                    row_index = row_index,
+                   top_n_features = top_n_features,
                    features = features,
+                   nonzeroCI = nonzeroCI,
                    plot = plot,
                    print = print)
 }
